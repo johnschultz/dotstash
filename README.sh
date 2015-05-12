16f47c141d30df41e37693eba1b5a70fd577b88f
Johnathon Schultz <5789141+johnschultz@users.noreply.github.com>
2015-05-11 17:45:23 -0700

    Add vundle submodule

--- README.sh
+++ README.sh
@@ -1,5 +1,6 @@
 DIRNAME=$(python -c "import os,sys; print os.path.realpath(sys.argv[1])" $(dirname $0))
 git submodule update --init --recursive
+ln -s vim/Vundle.vim vim/bundle/
 
 cp profile $HOME/.profile
 cp ackrc $HOME/.ackrc
@@ -16,3 +17,4 @@ ln -s $DIRNAME/vim .vim
 ln -s .vim/vimrc .vimrc
 
 echo "Don't forget to source ~/.profile"
+echo "Don't forget to :PluginInstall in Vim"
